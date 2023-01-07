70166e0bff5e9fabb561a437bc261db764a6636c
Sankhadeep Chatterjee <sankhadeep2007@gmail.com>
2023-01-07 15:37:56 +0530

    folder war added

---
+++ src/main/java/com/sankha/twitter/SpringJenkinsMavenWarApplication.java
@@ -0,0 +1,13 @@
+package com.sankha.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsMavenWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsMavenWarApplication.class, args);
+	}
+
+}
